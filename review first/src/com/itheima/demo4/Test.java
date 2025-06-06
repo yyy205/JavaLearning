a60717fd31b83fe3e9b81e84c202630baf200971
yyy205 <tianhao100804@gmail.com>
2025-06-06 19:46:29 +0800

    third

--- review first/src/com/itheima/demo4/Test.java
+++ review first/src/com/itheima/demo4/Test.java
@@ -2,22 +2,9 @@ package com.itheima.demo4;
 
 public class Test {
     public static void main(String[] args) {
-        Swim s1=new Swim() {
-            @Override
-            public void swimming() {
-                System.out.println("游泳kuai");
-            }
+        Swim s1 = () -> {
+            System.out.println("游泳kuai");
         };
-        start(s1);
-        Swim s2 = new Swim() {
-            @Override
-            public void swimming() {
-                System.out.println("游泳慢");
-            }
-        };
-        start(s2);
-    }
-    public static void start(Swim s) {
-            s.swimming();
+        s1.swimming();
     }
 }
